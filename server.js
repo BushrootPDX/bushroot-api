e2c5026e57aed168bd1594090053af4529c7d739
Mark Petrie <petrie.mark@gmail.com>
2017-09-14 16:35:16 -0700

    Add server.js and connect.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+require('.env').config();
+const app = require('./lib/app');
+const connect = require('./lib/connect');
+connect('mongodb://localhost:27017/bushroot'); // convert to process.env.mongodb_uri
+const server = http.createServer(app);
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+    console.log('server is running on', server.address().port);
+});
