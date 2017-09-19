341d7b8ff53b552604156c38bedf783a5aac9483
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-09-19 13:57:54 -0700

    convert to process.env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 require('dotenv').config();
 const app = require('./lib/app');
 const connect = require('./lib/connect');
-connect('mongodb://localhost:27017/bushroot'); // convert to process.env.mongodb_uri
+connect(process.env.MONGODB_URI);
 const server = http.createServer(app);
 const port = process.env.PORT || 3001;
 
