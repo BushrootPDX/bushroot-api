336b6d07e3c24cd2add769511502298fcd3d741a
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-09-19 14:09:21 -0700

    remove db require from garden model, fix duplicate proxy

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const server = http.createServer(app);
 const port = process.env.PORT || 3001;
 
 server.listen(port, () => {
-    console.log('server is running on', server.address().port);
+    console.log('server is running on', server.address().port); //eslint-disable-line
 });
